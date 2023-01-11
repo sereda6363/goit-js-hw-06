3167431a21b2fe2e0e98a1f0d7354cf087df415b
Stelmashchuk Sergii <sereda636322@gmail.com>
2023-01-11 21:40:38 +0200

    finished homework #1

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+const categories = document.querySelectorAll(".item");
+console.log(`Number of categories: ${categories.length}`);
+
+categories.forEach(element => {
+  console.log(`Category: ${element.firstElementChild.textContent}`);
+  console.log(`Elements: ${element.lastElementChild.children.length}`);
+});
