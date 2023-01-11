70c89f936b0e2f456c3c3127d25861c1168ee275
Stelmashchuk Sergii <sereda636322@gmail.com>
2023-01-12 01:00:13 +0200

    finished task#2 (hw#6)

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,30 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.querySelector("ul")
+
+for (const ingredient of ingredients) {
+  const element = document.createElement("li");
+  element.classList.add("item");
+  element.textContent = ingredient;
+  listEl.appendChild(element);
+}
+console.log(listEl)
+
+
+// Второй способ решения (map)
+
+// const listContentEl = ingredients
+//   .map((ingredient) => `<li class="item">${ingredient}</li>`)
+//   .join("");
+// listEl.insertAdjacentHTML("beforeend", listContentEl);
+
+// console.log(listEl)
+
+// // Третий способ решения (reduce)
+
+// const listContentEl = ingredients.reduce(
+//   (acc, ingredient) => (acc += `<li class="item">${ingredient}</li>`), "");
+//   listEl.insertAdjacentHTML("beforeend", listContentEl);
+// console.log(listEl)
