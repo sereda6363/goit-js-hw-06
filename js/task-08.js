27a34b1191f9f44a67ef549c9e768190fbe9e01c
Stelmashchuk Sergii <sereda636322@gmail.com>
2023-01-16 00:15:35 +0200

    finished task#8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const formEl = document.querySelector('.login-form');
+
+formEl.addEventListener('submit', event=> {
+  event.preventDefault()
+
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+
+
+  if (email.value === '' || password === '') {
+    return alert ('All fields must be filled!!!')
+  }
+
+  const userData = { email: email.value, password: password.value }
+  console.log(userData)
+
+  event.currentTarget.reset()
+})
 
