6b10b6948d6ed0b20b2604355d6bc03c55a13e92
Stelmashchuk Sergii <sereda636322@gmail.com>
2023-01-16 02:48:28 +0200

    finished task #9

--- js/task-01.js
+++ js/task-01.js
@@ -2,6 +2,6 @@ const categories = document.querySelectorAll(".item");
 console.log(`Number of categories: ${categories.length}`);
 
 categories.forEach(element => {
-  console.log(`Category: ${element.firstElementChild.textContent}`);
-  console.log(`Elements: ${element.lastElementChild.children.length}`);
+  console.log(`Category: ${element.firstElementChild.textContent}`)
+  console.log(`Elements: ${element.lastElementChild.children.length}`)
 });
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
-function getRandomHexColor() {
+const collorButtonEl = document.querySelector('.change-color');
+const spanCollorEl = document.querySelector('.color');
+
+collorButtonEl.addEventListener('click', () => {
+  let funEl = getRandomHexColor()
+  document.body.style.backgroundColor = funEl
+  spanCollorEl.textContent = funEl
+})
+
+const getRandomHexColor= () => {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
